db83a7f66ab7712b3f575f1ddec5d2f2f8241d17
Tien Truong <98truongtien@gmail.con>
2022-11-14 20:02:39 +0700

    Update news.js

--- scripts/news.js
+++ scripts/news.js
@@ -10,12 +10,7 @@ const pageNum = document.getElementById('page-num');
 const USER_KEY = 'CURRENT_USER';
 const currentUser = JSON.parse(getFromStorage(USER_KEY)) ?? [];
 // console.log(currentUser);//for check error
-let pageSize;
-if (currentUser == null) {
-  pageSize = 10;
-} else {
-  pageSize = currentUser[0].pageSize;
-}
+const pageSize = currentUser[0].pageSize;
 let page = 1;
 let totalArticles;
 
